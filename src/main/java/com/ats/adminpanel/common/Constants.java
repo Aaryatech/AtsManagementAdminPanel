29b156946e1a42e89f907e5a2829a9c7b2aa3581
Viplav Thoke <viplavthoke@gmail.com>
2018-04-25 11:23:55 +0530

    /Akshay //Initialize...

---
+++ src/main/java/com/ats/adminpanel/common/Constants.java
@@ -0,0 +1,10 @@
+package com.ats.adminpanel.common;
+ 
+
+public class Constants {
+	
+ 
+	public static final String url="http://localhost:8093/";
+	//public static final String ReportURL = "http://localhost:8080/project/";
+	 
+}
