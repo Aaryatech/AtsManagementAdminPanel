929810fc5458c99f628cbbc5716b2ac979cf1b2f
ATS <ATS@192.168.2.4>
2018-04-28 17:53:35 +0530

    //Neha

--- src/main/java/com/ats/adminpanel/HomeController.java
+++ src/main/java/com/ats/adminpanel/HomeController.java
@@ -1,8 +1,12 @@
 package com.ats.adminpanel;
 
 import java.io.IOException;
+
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,40 +19,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ats.adminpanel.common.Constants;
+import com.ats.adminpanel.model.GetTask;
+import com.ats.adminpanel.model.GetTaskList;
 import com.ats.adminpanel.model.LoginResponse;
- 
+import com.ats.adminpanel.model.Task;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
+	RestTemplate rest = new RestTemplate();
+
+	List<GetTask> assignedTask = new ArrayList<GetTask>();
+	List<GetTask> inprogress = new ArrayList<GetTask>();
+	List<GetTask> completed = new ArrayList<GetTask>();
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "login";
 	}
-	
+
 	@RequestMapping("/loginProcess")
 	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse res) throws IOException {
 
@@ -57,7 +67,7 @@ public class HomeController {
 
 		ModelAndView mav = new ModelAndView("login");
 
-		res.setContentType("text/html"); 
+		res.setContentType("text/html");
 		try {
 			System.out.println("Login Process " + name);
 			System.out.println("password " + password);
@@ -67,28 +77,23 @@ public class HomeController {
 				mav = new ModelAndView("login");
 			} else {
 
-				  
-				String pass = "1234"; 
+				String pass = "1234";
 				RestTemplate rest = new RestTemplate();
-				MultiValueMap<String, Object> map = new LinkedMultiValueMap<String,Object>();
+				MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
 				map.add("empMobile", name);
 				map.add("empPwd", password);
-				LoginResponse loginResponse = rest.postForObject(Constants.url + "/masters/login",map,
+				LoginResponse loginResponse = rest.postForObject(Constants.url + "/masters/login", map,
 						LoginResponse.class);
 				System.out.println("loginResponse" + loginResponse);
-				 
-				if (loginResponse.isError()==false) 
-				{ 
+
+				if (loginResponse.isError() == false) {
 					mav = new ModelAndView("home");
 					HttpSession session = request.getSession();
-					
-					session.setAttribute("employee", loginResponse.getEmployee());
 					session.setAttribute("UserDetail", loginResponse);
-					 
+
 				} else {
 
-					
-					mav = new ModelAndView("login"); 
+					mav = new ModelAndView("login");
 					System.out.println("Invalid login credentials");
 
 				}
@@ -102,13 +107,130 @@ public class HomeController {
 		return mav;
 
 	}
-	
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public String logout(HttpSession session) {
-		System.out.println("User Logout");
 
-		session.invalidate();
-		return "redirect:/";
+	@RequestMapping(value = "/homePage", method = RequestMethod.GET)
+	public ModelAndView viewBill(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/homePage");
+		List<GetTask> getTaskList = new ArrayList<GetTask>();
+		try {
+
+			// int developerId = Integer.parseInt(request.getParameter("developerId"));
+			MultiValueMap<String, Object> map = new LinkedMultiValueMap<String, Object>();
+			map.add("developerId", "1");
+			GetTask[] taskList = rest.postForObject(Constants.url + "masters/allTaskByDeveloperId", map,
+					GetTask[].class);
+
+			getTaskList = new ArrayList<GetTask>(Arrays.asList(taskList));
+
+			System.out.println("task list size " + getTaskList.size());
+			assignedTask.clear();
+			inprogress.clear();
+			completed.clear();
+
+			// System.out.println("getTaskList ----"+getTaskList);
+
+			for (int i = 0; i < getTaskList.size(); i++) {
+				if (getTaskList.get(i).getDevStatus() == 1) {
+
+					assignedTask.add(getTaskList.get(i));
+
+					System.out.println("Assigned Task ----" + assignedTask.add(getTaskList.get(i)));
+				} else if (getTaskList.get(i).getDevStatus() == 2) {
+
+					inprogress.add(getTaskList.get(i));
+				} else if (getTaskList.get(i).getDevStatus() == 3) {
+
+					completed.add(getTaskList.get(i));
+				}
+
+			}
+			model.addObject("assignedCount", assignedTask.size());
+			model.addObject("inprogressCount", inprogress.size());
+			model.addObject("completedCount", completed.size());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return model;
+
 	}
-	
+
+	@RequestMapping(value = "/showAssignedTask", method = RequestMethod.GET)
+	public ModelAndView assignedTask(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/assignedTaskList");
+
+		model.addObject("assignedTask", assignedTask);
+		return model;
+
+	}
+
+	@RequestMapping(value = "/assignedTaskDetails/{taskId}", method = RequestMethod.GET)
+	public ModelAndView assignedTaskDetails(@PathVariable int taskId) {
+		ModelAndView model = new ModelAndView("project/assignedTaskDetails");
+		RestTemplate restTemplate = new RestTemplate();
+		MultiValueMap<String, Object> vars = new LinkedMultiValueMap<String, Object>();
+
+		vars.add("taskId", taskId);
+
+		GetTaskList taskList = restTemplate.postForObject(Constants.url + "/masters/getTaskDetailsByTaskId", vars,
+				GetTaskList.class);
+		// masters/getTaskDetailsByTaskId
+		model.addObject("taskList", taskList);
+		return model;
+	}
+
+	@RequestMapping(value = "/showTaskDeatils", method = RequestMethod.GET)
+	public ModelAndView showtaskDeatils(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/taskDetails");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/showInprogessPage", method = RequestMethod.GET)
+	public ModelAndView showInprogessPage(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/inprogressList");
+
+		model.addObject("inprogress", inprogress);
+		return model;
+
+	}
+
+	@RequestMapping(value = "/inprogressTaskDetails/{taskId}", method = RequestMethod.GET)
+	public ModelAndView inprogressTaskDetails(@PathVariable int taskId) {
+		ModelAndView model = new ModelAndView("project/inprogressTaskDetails");
+		RestTemplate restTemplate = new RestTemplate();
+		MultiValueMap<String, Object> vars = new LinkedMultiValueMap<String, Object>();
+
+		vars.add("taskId", taskId);
+
+		GetTask taskList = restTemplate.postForObject(Constants.url + "/masters/getTaskDetailsByTaskId", vars,
+				GetTask.class);
+		// masters/getTaskDetailsByTaskId
+		model.addObject("taskList", taskList);
+		return model;
+	}
+
+	@RequestMapping(value = "/showForwardPage", method = RequestMethod.GET)
+	public ModelAndView showForwardPage(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/forward");
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/showCompletedPage", method = RequestMethod.GET)
+	public ModelAndView showCompletedPage(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("project/completed");
+
+		model.addObject("completed", completed);
+		return model;
+
+	}
+
 }
---
+++ src/main/java/com/ats/adminpanel/model/GetTaskList.java
@@ -0,0 +1,203 @@
+package com.ats.adminpanel.model;
+
+public class GetTaskList {
+	private int taskId; 
+	private int projectId; 
+	private int moduleId; 
+	private int formId; 
+	private int taskTypeId; 
+	private String taskName; 
+	private String taskDescription; 
+	private String taskSpRemarks; 
+	private String taskPlannedHrs; 
+	private int developerId; 
+	private int testerId; 
+	private int assignedBy; 
+	private String startDate; 
+	private String startDatetime; 
+	private String devComplPer; 
+	private String endDate; 
+	private String endDatetime; 
+	private int devStatus; 
+	private String actualReqHrs; 
+	private int testerStatus; 
+	private String remarksByDev; 
+	private String projectName; 
+	private String moduleName; 
+	private String formName; 
+	private String taskTypeName;
+	
+	private String empName;
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+	public int getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+	public int getModuleId() {
+		return moduleId;
+	}
+	public void setModuleId(int moduleId) {
+		this.moduleId = moduleId;
+	}
+	public int getFormId() {
+		return formId;
+	}
+	public void setFormId(int formId) {
+		this.formId = formId;
+	}
+	public int getTaskTypeId() {
+		return taskTypeId;
+	}
+	public void setTaskTypeId(int taskTypeId) {
+		this.taskTypeId = taskTypeId;
+	}
+	public String getTaskName() {
+		return taskName;
+	}
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+	public String getTaskSpRemarks() {
+		return taskSpRemarks;
+	}
+	public void setTaskSpRemarks(String taskSpRemarks) {
+		this.taskSpRemarks = taskSpRemarks;
+	}
+	public String getTaskPlannedHrs() {
+		return taskPlannedHrs;
+	}
+	public void setTaskPlannedHrs(String taskPlannedHrs) {
+		this.taskPlannedHrs = taskPlannedHrs;
+	}
+	public int getDeveloperId() {
+		return developerId;
+	}
+	public void setDeveloperId(int developerId) {
+		this.developerId = developerId;
+	}
+	public int getTesterId() {
+		return testerId;
+	}
+	public void setTesterId(int testerId) {
+		this.testerId = testerId;
+	}
+	public int getAssignedBy() {
+		return assignedBy;
+	}
+	public void setAssignedBy(int assignedBy) {
+		this.assignedBy = assignedBy;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getStartDatetime() {
+		return startDatetime;
+	}
+	public void setStartDatetime(String startDatetime) {
+		this.startDatetime = startDatetime;
+	}
+	public String getDevComplPer() {
+		return devComplPer;
+	}
+	public void setDevComplPer(String devComplPer) {
+		this.devComplPer = devComplPer;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getEndDatetime() {
+		return endDatetime;
+	}
+	public void setEndDatetime(String endDatetime) {
+		this.endDatetime = endDatetime;
+	}
+	public int getDevStatus() {
+		return devStatus;
+	}
+	public void setDevStatus(int devStatus) {
+		this.devStatus = devStatus;
+	}
+	public String getActualReqHrs() {
+		return actualReqHrs;
+	}
+	public void setActualReqHrs(String actualReqHrs) {
+		this.actualReqHrs = actualReqHrs;
+	}
+	public int getTesterStatus() {
+		return testerStatus;
+	}
+	public void setTesterStatus(int testerStatus) {
+		this.testerStatus = testerStatus;
+	}
+	public String getRemarksByDev() {
+		return remarksByDev;
+	}
+	public void setRemarksByDev(String remarksByDev) {
+		this.remarksByDev = remarksByDev;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getModuleName() {
+		return moduleName;
+	}
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+	public String getFormName() {
+		return formName;
+	}
+	public void setFormName(String formName) {
+		this.formName = formName;
+	}
+	public String getTaskTypeName() {
+		return taskTypeName;
+	}
+	public void setTaskTypeName(String taskTypeName) {
+		this.taskTypeName = taskTypeName;
+	}
+
+	@Override
+	public String toString() {
+		return "GetTaskList [taskId=" + taskId + ", projectId=" + projectId + ", moduleId=" + moduleId + ", formId="
+				+ formId + ", taskTypeId=" + taskTypeId + ", taskName=" + taskName + ", taskDescription="
+				+ taskDescription + ", taskSpRemarks=" + taskSpRemarks + ", taskPlannedHrs=" + taskPlannedHrs
+				+ ", developerId=" + developerId + ", testerId=" + testerId + ", assignedBy=" + assignedBy
+				+ ", startDate=" + startDate + ", startDatetime=" + startDatetime + ", devComplPer=" + devComplPer
+				+ ", endDate=" + endDate + ", endDatetime=" + endDatetime + ", devStatus=" + devStatus
+				+ ", actualReqHrs=" + actualReqHrs + ", testerStatus=" + testerStatus + ", remarksByDev=" + remarksByDev
+				+ ", projectName=" + projectName + ", moduleName=" + moduleName + ", formName=" + formName
+				+ ", taskTypeName=" + taskTypeName + ", empName=" + empName + "]";
+	}
+	
+	
+
+}
